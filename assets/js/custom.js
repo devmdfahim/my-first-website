a5b84f12236f1bde8b8ebb6f76955276a4ab85f3
devmdfahim <developer.mdfahim@gmail.com>
2022-04-10 14:32:32 +0600

    My first portfolio project compleated!

---
+++ assets/js/custom.js
@@ -0,0 +1,20 @@
+(function($){
+    //Owl-Carousel Start
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:30,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:3
+            }
+        }
+    })
+
+}) (jQuery);
